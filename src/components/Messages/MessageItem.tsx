6d3302998dae438b0d1a740679ef4197553608b4
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-20 18:35:33 +0600

    Added laout and props to MessageItem comp

--- src/components/Messages/MessageItem.tsx
+++ src/components/Messages/MessageItem.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Message } from "../../types";
 
-const MessageItem = () => {
+interface Props {
+  message: Message;
+}
+
+const MessageItem: React.FC<Props> = ({ message }) => {
   return (
-    <div>
-      Message
+    <div className="row-1 my-2">
+      <div className="bg-primary text-white d-inline-block px-4 py-3 rounded-2">
+        <p className="m-0 text-break">{message.author}</p>
+        <p className="m-0 text-break">{message.message}</p>
+        <span className="d-block text-end small">{message.datetime}</span>
+      </div>
     </div>
   );
 };
