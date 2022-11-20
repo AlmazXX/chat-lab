3f7e7d36fd62aeffc1ffa307eded037897bdfff5
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-20 18:35:05 +0600

    Added layout  and Props to Messages comp

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Message } from "../../types";
+import MessageItem from "./MessageItem";
 
-const Messages = () => {
+interface Props {
+  messagesList: Message[];
+}
+
+const Messages: React.FC<Props> = ({ messagesList }) => {
   return (
-    <div>
-      Messages
+    <div className="bg-light p-3 my-3 rounded-2">
+      {messagesList.map((message) => (
+        <MessageItem key={message._id} message={message} />
+      ))}
     </div>
   );
 };
