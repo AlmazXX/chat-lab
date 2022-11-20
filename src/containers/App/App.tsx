cead2106607cc20d53c48b8ab1a92d54ccf5c296
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-20 15:30:46 +0600

    Created layout for the App

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 function App() {
   return (
     <React.Fragment>
-      App
+      <header>
+        <nav className="navbar navbar-light bg-primary text-white">
+          <div className="container"><a href="#" className="text-decoration-none"><span className="navbar-brand mb-0 h1 text-white">Chat</span></a></div>
+        </nav>
+      </header>
+      <main>
+        <div className="container py-3">
+          <div className="row">
+            Messages
+          </div>
+          <div className="row">Message form</div>
+        </div>
+      </main>
     </React.Fragment>
   );
 }
