844f6ad02692939a6d0cea3a1891b961007547f5
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-29 22:26:12 +0600

    Removed unnecessary imports Removed logic of hiding user's email after 1st message

--- src/components/MessageForm/MessageForm.tsx
+++ src/components/MessageForm/MessageForm.tsx
@@ -1,47 +1,42 @@
-import React, { useState } from "react";
+import { ChangeEvent, FC, FormEvent, useState } from "react";
 import { MessageMutation } from "../../types";
 
 interface Props {
   onSubmit: (message: string, author: string) => void;
 }
 
-const MessageForm: React.FC<Props> = ({ onSubmit }) => {
+const MessageForm: FC<Props> = ({ onSubmit }) => {
   const [message, setMessage] = useState<MessageMutation>({
     message: "",
     author: "",
   });
-  const [hasAuthor, setHasAuthor] = useState(false);
 
   const onFormChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
     setMessage((prev) => ({ ...prev, [name]: value }));
   };
 
-  const onFormSubmit = (e: React.FormEvent) => {
+  const onFormSubmit = (e: FormEvent) => {
     e.preventDefault();
     onSubmit(message.message, message.author);
     setMessage((prev) => ({ ...prev, message: "" }));
-    setHasAuthor(true);
   };
 
-  const authorSet = hasAuthor ? null : (
-    <div className="mb-3 d-flex flex-column">
-      <label htmlFor="author">Your email</label>
-      <input
-        type="email"
-        id="author"
-        className="form-control"
-        value={message.author}
-        name="author"
-        onChange={onFormChange}
-      />
-    </div>
-  );
   return (
     <form className="col-md-6" onSubmit={onFormSubmit}>
-      {authorSet}
+      <div className="mb-3 d-flex flex-column">
+        <label htmlFor="author">Your email</label>
+        <input
+          type="email"
+          id="author"
+          className="form-control"
+          value={message.author}
+          name="author"
+          onChange={onFormChange}
+        />
+      </div>
       <div className="mb-3 d-flex flex-column">
         <label htmlFor="message" className="form-label">
           Your message
